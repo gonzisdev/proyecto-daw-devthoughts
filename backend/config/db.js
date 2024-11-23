26850e84b0743de403eeb9d2d66ef634680cba88
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-11-23 10:37:53 +0100

    added por to db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,4 +8,5 @@ export const db = await mysql.createConnection({ // Creamos la conexion
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
 });
