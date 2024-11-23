321f458a6fc0b49b1543e8c746ddfda4c0294e31
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-11-23 13:04:56 +0100

    removed unused frontend versions

--- frontend - 1.0/hooks/useApp.js
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { AppContext } from "../context/AppContext";
-
-export const useApp = () => useContext(AppContext); // Creamos un hook propio con nuestro context, para mayor comodidad a la hora de programar y acceder al provider
--- frontend - 2.0/components/Alert.jsx
+++
@@ -1,9 +0,0 @@
-// Componente de alerta reutilizable via props, recibe un msg y un boolean indicando si hay error o no
-
-export const Alert = ({ alert }) => {
-	return (
-		<div className={`alert ${alert.error ? "danger" : "success"}`}>
-			{alert.msg}
-		</div>
-	);
-};
